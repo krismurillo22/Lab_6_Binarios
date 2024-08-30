1eed27988748fe1a97426cb0e5bc6d1fcd9719fc
krismurillo22 <kristian_murillo@unitec.edu>
2024-08-30 13:08:10 -0600

    Subida

---
+++ src/lab_6_binarios/Lab_6_Binarios.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_6_binarios;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_6_Binarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
